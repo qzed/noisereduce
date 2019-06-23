99e29e4033a94e2614d0c6f282e9eae74195ee95
qzed <qzed@users.noreply.github.com>
2019-06-23 19:30:03 +0200

    Add array padding functions

--- src/ft.rs
+++ src/ft.rs
@@ -11,7 +11,7 @@ pub fn magnitude_to_db<F: Float>(m: F) -> F {
 pub fn fftshift<D, T>(input: &ArrayBase<D, Ix1>) -> Array1<T>
 where
     D: Data<Elem = T>,
-    T: Copy + Zero,
+    T: Clone + Zero,
 {
     let mut output = Array1::zeros(input.len());
     fftshift_into(input, &mut output);
@@ -22,7 +22,7 @@ pub fn fftshift_into<D, M, T>(input: &ArrayBase<D, Ix1>, output: &mut ArrayBase<
 where
     D: Data<Elem = T>,
     M: DataMut<Elem = T>,
-    T: Copy,
+    T: Clone,
 {
     output.slice_mut(s![0..input.len()/2]).assign(&input.slice(s![(input.len()+1)/2..]));
     output.slice_mut(s![input.len()/2..]).assign(&input.slice(s![..(input.len()+1)/2]));
@@ -31,7 +31,7 @@ where
 pub fn ifftshift<D, T>(input: &ArrayBase<D, Ix1>) -> Array1<T>
 where
     D: Data<Elem = T>,
-    T: Copy + Zero,
+    T: Clone + Zero,
 {
     let mut output = Array1::zeros(input.len());
     ifftshift_into(&input.view(), &mut output.view_mut());
@@ -42,7 +42,7 @@ pub fn ifftshift_into<D, M, T>(input: &ArrayBase<D, Ix1>, output: &mut ArrayBase
 where
     D: Data<Elem = T>,
     M: DataMut<Elem = T>,
-    T: Copy,
+    T: Clone,
 {
     output.slice_mut(s![0..(input.len()+1)/2]).assign(&input.slice(s![input.len()/2..]));
     output.slice_mut(s![(input.len()+1)/2..]).assign(&input.slice(s![..input.len()/2]));
@@ -66,6 +66,103 @@ where
     output.slice_mut(s![..center]).assign(&Array1::range(F::zero(), F::from(center).unwrap(), F::one()));
     output.slice_mut(s![center..]).assign(&Array1::range(-F::from(n / 2).unwrap(), F::zero(), F::one()));
     output.mapv_inplace(|v| v * scale);
+}
+
+
+pub fn extend_zero<D, F>(input: &ArrayBase<D, Ix1>, n: usize) -> Array1<F>
+where
+    D: Data<Elem = F>,
+    F: Clone + Zero,
+{
+    let mut result = Array1::zeros(input.len() + 2 * n);
+    extend_zero_into(n, input, &mut result);
+    result
+}
+
+pub fn extend_zero_into<D, M, F>(n: usize, input: &ArrayBase<D, Ix1>, output: &mut ArrayBase<M, Ix1>)
+where
+    D: Data<Elem = F>,
+    M: DataMut<Elem = F>,
+    F: Clone + Zero,
+{
+    output.slice_mut(s![0..n]).fill(F::zero());
+    output.slice_mut(s![n..n+input.len()]).assign(&input);
+    output.slice_mut(s![n+input.len()..]).fill(F::zero());
+}
+
+pub fn extend_const<D, F>(input: &ArrayBase<D, Ix1>, n: usize) -> Array1<F>
+where
+    D: Data<Elem = F>,
+    F: Clone + Zero,
+{
+    let mut result = Array1::zeros(input.len() + 2 * n);
+    extend_const_into(n, input, &mut result);
+    result
+}
+
+pub fn extend_const_into<D, M, F>(n: usize, input: &ArrayBase<D, Ix1>, output: &mut ArrayBase<M, Ix1>)
+where
+    D: Data<Elem = F>,
+    M: DataMut<Elem = F>,
+    F: Clone,
+{
+    let a = input[0].clone();
+    let b = input[input.len()-1].clone();
+
+    output.slice_mut(s![0..n]).fill(a);
+    output.slice_mut(s![n..n+input.len()]).assign(&input);
+    output.slice_mut(s![n+input.len()..]).fill(b);
+}
+
+pub fn extend_even<D, F>(input: &ArrayBase<D, Ix1>, n: usize) -> Array1<F>
+where
+    D: Data<Elem = F>,
+    F: Clone + Zero,
+{
+    let mut result = Array1::zeros(input.len() + 2 * n);
+    extend_even_into(n, input, &mut result);
+    result
+}
+
+pub fn extend_even_into<D, M, F>(n: usize, input: &ArrayBase<D, Ix1>, output: &mut ArrayBase<M, Ix1>)
+where
+    D: Data<Elem = F>,
+    M: DataMut<Elem = F>,
+    F: Clone,
+{
+    output.slice_mut(s![0..n]).assign(&input.slice(s![1..n+1; -1]));
+    output.slice_mut(s![n..n+input.len()]).assign(&input);
+    output.slice_mut(s![n+input.len()..]).assign(&input.slice(s![input.len()-n-1..input.len()-1; -1]));
+}
+
+pub fn extend_odd<D, F>(input: &ArrayBase<D, Ix1>, n: usize) -> Array1<F>
+where
+    D: Data<Elem = F>,
+    F: Clone + Zero + std::ops::Add<Output=F> + std::ops::SubAssign,
+{
+    let mut result = Array1::zeros(input.len() + 2 * n);
+    extend_odd_into(n, input, &mut result);
+    result
+}
+
+pub fn extend_odd_into<D, M, F>(n: usize, input: &ArrayBase<D, Ix1>, output: &mut ArrayBase<M, Ix1>)
+where
+    D: Data<Elem = F>,
+    M: DataMut<Elem = F>,
+    F: Clone + std::ops::Add<Output=F> + std::ops::SubAssign,
+{
+    let a = input[0].clone() + input[0].clone();
+    let b = input[input.len()-1].clone() + input[input.len()-1].clone();
+
+    let mut ext_a = output.slice_mut(s![0..n]);
+    ext_a.fill(a);
+    ext_a -= &input.slice(s![1..n+1; -1]);
+
+    output.slice_mut(s![n..n+input.len()]).assign(&input);
+
+    let mut ext_b = output.slice_mut(s![n+input.len()..]);
+    ext_b.fill(b);
+    ext_b -= &input.slice(s![input.len()-n-1..input.len()-1; -1]);
 }
 
 
