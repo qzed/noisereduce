f586559b8de3125afd4ea2701f16bd3f30cf7b96
qzed <qzed@users.noreply.github.com>
2019-06-25 04:16:27 +0200

    Implement proper inverse STFT

--- examples/istft.rs
+++ examples/istft.rs
@@ -1,19 +1,19 @@
 use sspse::wave::WavReaderExt;
-use sspse::window::{self as W, WindowFunction};
+use sspse::window as W;
 use sspse::ft;
 
 use hound::{WavReader, Error};
-use num::{Complex, traits::Zero};
-use ndarray::{s, Axis, Array1};
-use rustfft::{FFTplanner};
-use gnuplot::{Figure, AxesCommon, AutoOption};
+use num::Complex;
+use ndarray::Axis;
+use gnuplot::Figure;
 
 
 fn main() -> Result<(), Error> {
-    let path = std::env::args_os().nth(1).expect("missing file argument");
+    let path_in = std::env::args_os().nth(1).expect("missing input file argument");
+    let path_out = std::env::args_os().nth(2).expect("missing output file argument");
 
     // load first channel of wave file
-    let (samples, samples_spec) = WavReader::open(path)?.into_array_f32()?;
+    let (samples, samples_spec) = WavReader::open(path_in)?.into_array_f32()?;
     let samples = samples.index_axis_move(Axis(1), 0);
 
     // convert real to complex
@@ -23,9 +23,6 @@ fn main() -> Result<(), Error> {
     let fft_len     = 512;
     let segment_len = 256;
     let overlap     = 192;
-
-    let method = ft::InversionMethod::Weighted;
-    let padded = true;
 
     // build window for fft
     let window = W::periodic(W::sqrt(W::hann(segment_len)));
@@ -38,72 +35,22 @@ fn main() -> Result<(), Error> {
         .overlap(overlap)
         .padding(ft::Padding::Zero)
         .build();
+    
+    let mut istft = ft::IstftBuilder::with_len(&window, fft_len)
+        .overlap(overlap)
+        .method(ft::InversionMethod::Weighted)
+        .remove_padding(true)
+        .build();
 
-    let spectrum = stft.forward(&samples_c);
-
-    // compute inverse STFT
-    let window = window.to_array();
-
-    let a = match method {
-        ft::InversionMethod::Weighted => 1,
-        ft::InversionMethod::Basic    => 0,
-    };
-
-    let hop_ratio = (segment_len - 166) as f32 / ((segment_len - 192) as f32);
-    let overlap = 166;
-    let new_sample_rate = (samples_spec.sample_rate as f32 * hop_ratio) as u32;
-
-    let num_segments = spectrum.shape()[0];
-    let step_len = segment_len - overlap;
-    let len = segment_len + (num_segments - 1) * step_len;
-
-    let mut out: Array1<Complex<f32>> = Array1::zeros(len);
-    let mut norm: Array1<f32> = Array1::zeros(len);
-
-    let mut buf_in: Array1<Complex<f32>>  = Array1::zeros(fft_len);
-    let mut buf_out: Array1<Complex<f32>> = Array1::zeros(fft_len);
-
-    // reset arrays
-    out.fill(Complex::zero());
-    norm.fill(0.0);
-
-    let ifft = FFTplanner::new(true).plan_fft(fft_len);
-    let fftnorm = 1.0 / fft_len as f32;
-
-    // overlap and add
-    for i in 0..num_segments {
-        buf_in.assign(&spectrum.slice(s![i, ..]));
-
-        ifft.process(buf_in.as_slice_mut().unwrap(), buf_out.as_slice_mut().unwrap());
-
-        // handle fft and segment length difference (remove padding)
-        let s = (fft_len - segment_len) / 2;
-        let out_unpadded = buf_out.slice(s![s..s+segment_len]);
-
-        // (weighted) overlap+add (+ fft normalization)
-        for (j, v) in out_unpadded.indexed_iter() {
-            out[i * step_len + j]  += v * fftnorm * window[j].powi(a);
-            norm[i * step_len + j] += window[j].powi(a + 1);
-        }
-    }
-
-    // remove data introduced by padding in forward STFT
-    if padded {
-        out.slice_collapse(s![segment_len/2..out.len()-segment_len/2]);
-        norm.slice_collapse(s![segment_len/2..norm.len()-segment_len/2]);
-    }
-
-    // normalization
-    for (i, v) in out.indexed_iter_mut() {
-        if norm[i] > 1e-10 { *v = *v / norm[i]; }
-    }
+    // perform stft and istft
+    let out = istft.process(&stft.process(&samples_c));
 
     // drop imaginary part
-    let out = out.mapv(|v| v.re * hop_ratio);
+    let out = out.mapv(|v| v.re);
 
     // plot
     let tx_s = ft::sample_times(samples.len(), samples_spec.sample_rate as f64);
-    let tx_o = ft::sample_times(out.len(), new_sample_rate as f64);
+    let tx_o = ft::sample_times(out.len(), samples_spec.sample_rate as f64);
 
     let mut fig = Figure::new();
     let ax = fig.axes2d();
@@ -114,12 +61,12 @@ fn main() -> Result<(), Error> {
     // write
     let out_spec = hound::WavSpec {
         channels: 1,
-        sample_rate: new_sample_rate,
+        sample_rate: samples_spec.sample_rate,
         bits_per_sample: 32,
         sample_format: hound::SampleFormat::Float,
     };
 
-    let mut writer = hound::WavWriter::create("out.wav", out_spec)?;
+    let mut writer = hound::WavWriter::create(path_out, out_spec)?;
     for x in out.iter() {
         writer.write_sample(*x)?;
     }
--- src/ft.rs
+++ src/ft.rs
@@ -6,7 +6,7 @@ use std::sync::Arc;
 
 use num::Complex;
 use num::traits::{Float, Zero, NumAssign};
-use ndarray::{s, ArrayBase, Array1, Array2, Axis, Ix1, Ix2, Data, DataMut};
+use ndarray::{s, ArrayBase, Array1, Array2, Axis, Ix1, Ix2, Data, DataMut, ViewRepr};
 use rustfft::{FFT, FFTnum, FFTplanner};
 
 
@@ -303,7 +303,6 @@ pub struct StftBuilder<T> {
     window:  Array1<T>,
     overlap: Option<usize>,
     shifted: bool,
-    inverse: InversionMethod,
 }
 
 impl<T> StftBuilder<T>
@@ -334,7 +333,6 @@ where
             window:  window.to_array(),
             overlap: None,
             shifted: false,
-            inverse: InversionMethod::Weighted,
         }
     }
 
@@ -353,11 +351,6 @@ where
         self
     }
 
-    pub fn inversion_method(mut self, inverse: InversionMethod) -> Self {
-        self.inverse = inverse;
-        self
-    }
-
     pub fn build(self) -> Stft<T> {
         let len_fft = self.fft.len();
         let len_segment = self.window.len();
@@ -367,14 +360,13 @@ where
         assert!(len_fft >= len_segment);
 
         Stft {
-            fft:     self.fft,
-            window:  self.window,
-            padding: self.padding,
-            shifted: self.shifted,
-            inverse: self.inverse,
-            buf_in:  Array1::zeros(len_fft),
-            buf_out: Array1::zeros(len_fft),
-            buf_pad: Vec::new(),
+            fft:      self.fft,
+            window:   self.window,
+            padding:  self.padding,
+            shifted:  self.shifted,
+            buf_in:   Array1::zeros(len_fft),
+            buf_out:  Array1::zeros(len_fft),
+            buf_pad:  Vec::new(),
             len_fft, len_segment, len_overlap
         }
     }
@@ -386,7 +378,6 @@ pub struct Stft<T> {
     len_overlap: usize,
     len_segment: usize,
     shifted:     bool,
-    inverse:     InversionMethod,
     window:      Array1<T>,
     padding:     Padding,
     buf_in:      Array1<Complex<T>>,
@@ -396,7 +387,7 @@ pub struct Stft<T> {
 
 impl<T> Stft<T>
 where
-    T: FFTnum + NumAssign,
+    T: FFTnum + Float + NumAssign,
 {
     pub fn len_segment(&self) -> usize {
         self.len_segment
@@ -410,21 +401,21 @@ where
         self.len_overlap
     }
 
-    pub fn len_output_forward(&self, input_len: usize) -> usize {
-        let input_len = if self.padding != Padding::None {
-            input_len + 2 * (self.len_segment / 2)
+    pub fn len_output(&self, num_samples: usize) -> usize {
+        let num_samples = if self.padding != Padding::None {
+            num_samples + 2 * (self.len_segment / 2)
         } else {
-            input_len
+            num_samples
         };
 
-        (input_len - self.len_overlap) / (self.len_segment - self.len_overlap)
+        (num_samples - self.len_overlap) / (self.len_segment - self.len_overlap)
     }
 
-    pub fn forward_times<F: Float>(&self, input_len: usize, sample_rate: F) -> Array1<F> {
+    pub fn spectrum_times<F: Float>(&self, input_len: usize, sample_rate: F) -> Array1<F> {
         stft_times(input_len, self.len_segment, self.len_overlap, self.padding != Padding::None, sample_rate)
     }
 
-    pub fn forward_freqs<F: Float>(&self, sample_rate: F) -> Array1<F> {
+    pub fn spectrum_freqs<F: Float>(&self, sample_rate: F) -> Array1<F> {
         if self.shifted {
             fftshift(&fftfreq(self.len_fft, sample_rate))
         } else {
@@ -432,22 +423,22 @@ where
         }
     }
 
-    pub fn forward<D>(&mut self, input: &ArrayBase<D, Ix1>) -> Array2<Complex<T>>
+    pub fn process<D>(&mut self, input: &ArrayBase<D, Ix1>) -> Array2<Complex<T>>
     where
         D: Data<Elem = Complex<T>>,
     {
-        let mut output = Array2::zeros((self.len_output_forward(input.len()), self.len_fft));
-        self.forward_into(input, &mut output);
+        let mut output = Array2::zeros((self.len_output(input.len()), self.len_fft));
+        self.process_into(input, &mut output);
         output
     }
 
-    pub fn forward_into<D, M>(&mut self, input: &ArrayBase<D, Ix1>, output: &mut ArrayBase<M, Ix2>)
+    pub fn process_into<D, M>(&mut self, input: &ArrayBase<D, Ix1>, output: &mut ArrayBase<M, Ix2>)
     where
         D: Data<Elem = Complex<T>>,
         M: DataMut<Elem = Complex<T>>,
     {
         let step = self.len_segment - self.len_overlap;
-        let n_seg = self.len_output_forward(input.len());
+        let n_seg = self.len_output(input.len());
 
         assert!(output.shape()[0] >= n_seg);
         assert!(output.shape()[1] >= self.len_fft);
@@ -495,6 +486,200 @@ where
             } else {
                 fftshift_into(&self.buf_out, &mut output.slice_mut(s![i, 0..self.len_fft]));
             }
+        }
+    }
+}
+
+
+pub struct IstftBuilder<T> {
+    ifft:           Arc<dyn FFT<T>>,
+    method:         InversionMethod,
+    remove_padding: bool,
+    window:         Array1<T>,
+    overlap:        Option<usize>,
+}
+
+impl<T> IstftBuilder<T>
+where
+    T: FFTnum,
+{
+    pub fn new<W>(window: &W) -> Self
+    where
+        W: WindowFunction<T>
+    {
+        Self::with_ifft(window, FFTplanner::new(false).plan_fft(window.len()))
+    }
+
+    pub fn with_len<W>(window: &W, fft_len: usize) -> Self
+    where
+        W: WindowFunction<T>
+    {
+        Self::with_ifft(window, FFTplanner::new(true).plan_fft(fft_len))
+    }
+
+    pub fn with_ifft<W>(window: &W, ifft: Arc<dyn FFT<T>>) -> Self
+    where
+        W: WindowFunction<T>
+    {
+        IstftBuilder {
+            ifft,
+            method:         InversionMethod::Weighted,
+            remove_padding: true,
+            window:         window.to_array(),
+            overlap:        None,
+        }
+    }
+
+    pub fn overlap(mut self, overlap: usize) -> Self {
+        self.overlap = Some(overlap);
+        self
+    }
+
+    pub fn method(mut self, method: InversionMethod) -> Self {
+        self.method = method;
+        self
+    }
+
+    pub fn remove_padding(mut self, remove_padding: bool) -> Self {
+        self.remove_padding = remove_padding;
+        self
+    }
+
+    pub fn build(self) -> Istft<T> {
+        let len_fft = self.ifft.len();
+        let len_segment = self.window.len();
+        let len_overlap = self.overlap.unwrap_or_else(|| (len_segment / 4) * 3);
+
+        assert!(len_overlap < len_segment);
+        assert!(len_fft >= len_segment);
+
+        Istft {
+            ifft:           self.ifft,
+            method:         self.method,
+            window:         self.window,
+            remove_padding: self.remove_padding,
+            buf_in:         Array1::zeros(len_fft),
+            buf_out:        Array1::zeros(len_fft),
+            buf_acc:        Vec::new(),
+            buf_norm:       Vec::new(),
+            len_fft, len_segment, len_overlap
+        }
+    }
+}
+
+pub struct Istft<T> {
+    ifft:           Arc<dyn FFT<T>>,
+    method:         InversionMethod,
+    len_fft:        usize,
+    len_overlap:    usize,
+    len_segment:    usize,
+    window:         Array1<T>,
+    remove_padding: bool,
+    buf_in:         Array1<Complex<T>>,
+    buf_out:        Array1<Complex<T>>,
+    buf_acc:        Vec<Complex<T>>,
+    buf_norm:       Vec<T>,
+}
+
+impl<T> Istft<T>
+where
+    T: FFTnum + Float + NumAssign,
+{
+    pub fn len_segment(&self) -> usize {
+        self.len_segment
+    }
+
+    pub fn len_fft(&self) -> usize {
+        self.len_fft
+    }
+
+    pub fn len_overlap(&self) -> usize {
+        self.len_overlap
+    }
+
+    pub fn len_output(&self, len_spectrum: usize) -> usize {
+        let step_len = self.len_segment - self.len_overlap;
+        let len = self.len_segment + (len_spectrum - 1) * step_len;
+
+        if self.remove_padding {
+            len - (self.len_segment / 2) * 2
+        } else {
+            len
+        }
+    }
+
+    pub fn sample_times<F: Float>(&self, len_spectrum: usize, sample_rate: F)
+        -> Array1<F>
+    {
+        sample_times(self.len_output(len_spectrum), sample_rate)
+    }
+
+    pub fn process<'a, 'b, D>(&mut self, input: &ArrayBase<D, Ix2>) -> Array1<Complex<T>>
+    where
+        D: Data<Elem = Complex<T>>,
+    {
+        let mut output = Array1::zeros(self.len_output(input.shape()[0]));
+        self.process_into(input, &mut output);
+        output
+    }
+
+    pub fn process_into<'a, 'b, D, M>(&mut self, input: &ArrayBase<D, Ix2>, output: &'a mut ArrayBase<M, Ix1>)
+    where
+        D: Data<Elem = Complex<T>>,
+        M: DataMut<Elem = Complex<T>> + 'a,
+    {
+        // calculate lenghts and offsets
+        let len_spectrum = input.shape()[0];
+        let step = self.len_segment - self.len_overlap;
+        let len_padded = self.len_segment + (len_spectrum - 1) * step;
+
+        let (len_unpadded, offset_padding) = if self.remove_padding {
+            (len_padded - (self.len_segment / 2) * 2, self.len_segment / 2)
+        } else {
+            (len_padded, 0)
+        };
+
+        // re-initializce accumulation and norm buffers to zero
+        self.buf_acc.clear();
+        self.buf_acc.resize(len_padded, Complex::zero());
+
+        self.buf_norm.clear();
+        self.buf_norm.resize(len_padded, T::zero());
+
+        // choose method
+        let a = match self.method {
+            InversionMethod::Weighted => 1,
+            InversionMethod::Basic    => 0,
+        };
+
+        // overlap and add
+        for i in 0..len_spectrum {
+            self.buf_in.assign(&input.slice(s![i, ..]));
+
+            // TODO: this needs to be inverse -> split into two structs
+            self.ifft.process(self.buf_in.as_slice_mut().unwrap(), self.buf_out.as_slice_mut().unwrap());
+
+            // handle fft and segment length difference (remove padding)
+            let s = (self.len_fft - self.len_segment) / 2;
+
+            // (weighted) overlap+add (+ fft normalization)
+            for (j, v) in self.buf_out.slice(s![s..s+self.len_segment]).indexed_iter() {
+                self.buf_acc[i * step + j]  += v * self.window[j].powi(a);
+                self.buf_norm[i * step + j] += self.window[j].powi(a + 1);
+            }
+        }
+
+        // copy, normalize and remove padding
+        let fftnorm = T::one() / T::from(self.len_fft).unwrap();
+        let eps = T::from(1e-10).unwrap();
+
+        for i in 0..len_unpadded {
+            let v = self.buf_acc[offset_padding + i];
+
+            let n = self.buf_norm[offset_padding + i];
+            let n = if n > eps { n } else { T::one() };
+
+            output[i] = v * fftnorm / n;
         }
     }
 }
