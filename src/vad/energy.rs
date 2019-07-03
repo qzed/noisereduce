72e9693a7b2016676ba57189d65f3ef56559e3b7
qzed <qzed@users.noreply.github.com>
2019-07-03 19:45:03 +0200

    Add comments

--- src/vad/energy.rs
+++ src/vad/energy.rs
@@ -1,15 +1,30 @@
+//! Energy based voice activity detectors.
+
 use super::VoiceActivityDetector;
 
 use ndarray::{ArrayBase, Data, Ix1, Ix2};
 use num::{Complex, Float};
 
 
+/// Voice activity detector based on average spectrum energy.
+/// 
+/// Uses a noise-floor estimate (average energy) and a ratio to discriminate
+/// noise from a signal. If the average energy `e` of the given spectrum is
+/// larger than the threshold computed via `floor * ratio` the spectrum is
+/// classified as speech, otherwise it is classified as noise, i.e. the value
+/// returned by this detector is `e > floor * ratio`.
 pub struct EnergyThresholdVad<T> {
     floor: T,
     ratio: T,
 }
 
 impl<T> EnergyThresholdVad<T> {
+    /// Creates a new average-energy threshold voice activity detector.
+    /// 
+    /// - `floor` represents the noise-floor of the signal as average energy over
+    /// the spectrum.
+    /// - `ratio` is a ratio describing how much the average signal
+    /// energy may exceed the noise-floor before it is classified as speech.
     pub fn new(floor: T, ratio: T) -> Self {
         EnergyThresholdVad { floor, ratio }
     }
@@ -31,6 +46,12 @@ where
 }
 
 
+/// Compute an average-energy noise-floor estimation based on the given spectrum
+/// segments.
+/// 
+/// The spectrum segments are given as 2D array or slice with dimension `[n, m]`
+/// where `n` is the number of spectras to average over and `m` is the length of
+/// a single spectrum.
 pub fn noise_floor<T, D>(spectrum: &ArrayBase<D, Ix2>) -> T
 where
     T: Float,
--- src/vad/mod.rs
+++ src/vad/mod.rs
@@ -1,3 +1,5 @@
+//! Voice activity detection.
+
 use ndarray::{ArrayBase, Ix1, Data};
 use num::{Complex, Float};
 
@@ -6,16 +8,27 @@ pub mod energy;
 
 /// Voice activity detector with boolean output.
 pub trait VoiceActivityDetector<T> {
+    /// Returns `true` if voice activity has been detected in the given
+    /// spectrum, `false` otherwise.
     fn detect<D>(&self, spectrum: &ArrayBase<D, Ix1>) -> bool
     where
         D: Data<Elem=Complex<T>>;
 }
 
-pub trait VoicePresenceDetector<T> {
+/// Voice activity detector with probabilisitc output.
+pub trait VoicePresenceDetector<T>
+where
+    T: Float,
+{
+    /// Returns a probability-like measure describing the presence (`1.0`) or
+    /// absence (`0.0`) of voice in the given spectrum.
     fn detect<D>(&self, spectrum: &ArrayBase<D, Ix1>) -> T
     where
         D: Data<Elem=Complex<T>>;
 
+    /// Transforms this detector into a binary/boolean detector where any voice
+    /// presence probability returned by the original detector that is above the
+    /// given threshold is interpreted as present (`true`).
     fn binary(self, threshold: T) -> Binary<Self, T>
     where
         Self: Sized
@@ -25,7 +38,6 @@ pub trait VoicePresenceDetector<T> {
 }
 
 
-/// Voice activity detector with probabilisitc output.
 impl<V, T> VoicePresenceDetector<T> for V
 where
     V: VoiceActivityDetector<T>,
@@ -44,6 +56,9 @@ where
 }
 
 
+/// The voice activity detector returned by the
+/// [`binary`](VoicePresenceDetector::binary) method of a
+/// [`VoicePresenceDetector`](VoicePresenceDetector).
 pub struct Binary<B, T> {
     base: B,
     threshold: T,
